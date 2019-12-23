f675dd6038fff6362b9c504bd180bd9b69ebebd0
Madan <mmadan3600@gmail.com>
2019-12-23 13:46:07 +0545

    Initial Commit

---
+++ start-react/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import Usergreeting from './components/Usergreeting';
+// import Hello from './components/Hello'
+// import Welcome from './components/Welcome'
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick'
+// import ClassClick from './components/ClassClick'
+// import EventBinds from './components/EventBinds'
+// import ParentComponent from './components/ParentComponent'
+// import Namelist from './components/Namelist'
+  // import StyleSheet from './components/StyleSheet'
+  // import Inline from './components/Inline'
+  // import Form from './components/Form'
+  // import Fragment from './components/Fragment'
+  // import LIfecycleA from './components/LIfecycleA'
+  // import LIfecycleB from './components/LIfecycleB'
+  import Table from './components/Table'
+
+function App() {
+  return (
+    <div className="App">
+    <Table />
+    {/* <LIfecycleA /> */}
+    {/* <LIfecycleB /> */}
+    {/* <Fragment /> */}
+    {/* <Form /> */}
+    {/* <Inline /> */}
+    {/* <StyleSheet primary= {true} /> */}
+    {/* <Namelist /> */}
+      {/* <Usergreeting /> */}
+      {/* <Hello name="Brusly" heroName="Superman">
+        <p>This is children of hello</p>
+      </Hello>
+      <Hello name="Jaky Jan" heroName="Batsman" ><button>hello madan</button></Hello>
+      <Hello name="Hritik Roshan" heroName="PornStasr" /> */}
+      {/* <Welcome />  
+      <Jsxx /> */}
+      {/* <Message /> */}
+      {/* <Counter /> */}
+      {/* <ClassClick /> */}
+      {/* <EventBinds /> */}
+      {/* <ParentComponent /> */}
+    </div>
+  );
+}
+
+export default App;
